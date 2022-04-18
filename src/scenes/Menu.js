0a08e7100381ac9133afaa3360ab8f1336afa016
Margarita Rodriguez <mrodr157@ucsc.edu>
2022-04-18 11:37:30 -0700

    created menu and play scenes

---
+++ src/scenes/Menu.js
@@ -0,0 +1,42 @@
+class Menu extends Phaser.Scene {
+    constructor() {
+        super("menuScene");
+    }
+
+    preload() {
+        this.load.image('sky', './assets/sky.png');
+    }
+
+    create() {
+        this.opening = this.add.tileSprite(0, 0, 640, 480, 'sky').setOrigin(0,0);
+
+        //menu text config
+        let menuConfig = {
+            fontFamily: 'Courier',
+            fontSize: '28px',
+            backgroundColor: '#8a5a1c',
+            color: '#F3B141',
+            align: 'center',
+            padding: {
+                top: 5,
+                bottom: 5,
+            },
+            fixedWidth: 0
+        }
+
+        //menu text
+        this.add.text(game.config.width/2, game.config.height/2 - borderUISize - borderPadding, "THIS IS A GAME NAME", menuConfig).setOrigin(0.5);
+
+        //define keys
+        keySPACE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+    }
+
+    update() {
+
+        this.opening.tilePositionX -= 3;
+
+        if (Phaser.Input.Keyboard.JustDown(keySPACE)) {
+            this.scene.start('playScene');
+        }
+    }
+}
---
+++ src/scenes/Play.js
@@ -0,0 +1,33 @@
+class Play extends Phaser.Scene {
+    constructor() {
+        super("playScene");
+    }
+
+    preload() {
+        this.load.image('sky', './assets/sky.png')
+    }
+
+    create() {
+        this.sky = this.add.tileSprite(0, 0, 640, 480, 'sky').setOrigin(0,0);
+
+        let playConfig = {
+            fontFamily: 'Courier',
+            fontSize: '28px',
+            backgroundColor: '#000000',
+            color: '#F3B141',
+            align: 'center',
+            padding: {
+                top: 5, 
+                bottom: 5,
+            },
+            fixedWidth: 0
+        }
+
+        this.add.text(game.config.width/2, game.config.height/2 - borderUISize - borderPadding, 'THIS IS A PLAY SCENE', playConfig).setOrigin(0.5);
+    }
+
+    update() {
+
+        this.sky.tilePositionX -= 3;
+    }
+}
